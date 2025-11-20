8f285e7c86b156686e2d304d4ba1bb9a3763b08e
Stephen Njoroge <853stephen@gmail.com>
2025-11-20 15:24:07 +0300

    remove double url

--- client/src/pages/useDynamicAPI.jsx
+++ client/src/pages/useDynamicAPI.jsx
@@ -2,14 +2,14 @@ const useDynamicAPI = () => {
     const API = import.meta.env.VITE_API_URL.replace(/\/$/, '')
 
     const getAPI = async (endpoint) => {
-        const url = endpoint.startsWith('/') ?  `${API}${endpoint}` :  `${API}/${endpoint}`;
+        const url = endpoint.startsWith('http') ? endpoint : `${API}${endpoint}`;
         const response = await fetch(url, {headers: {'Content-Type' : 'application/json'}})
         if (!response.ok) throw new Error(`GET ${endpoint} Failed`);
         return response.json()
     }
 
     const postAPI = async(endpoint, body) => {
-        const url = endpoint.startsWith('/') ? `${API}${endpoint}` : `${API}/${endpoint}`;
+       const url = endpoint.startsWith('http') ? endpoint : `${API}${endpoint}`;
         const response = await fetch(url, {
             method: 'POST',
             headers: {'Content-Type' : 'application/json'},
@@ -20,7 +20,7 @@ const useDynamicAPI = () => {
     }
 
     const deleteAPI = async (endpoint) => {
-        const url = endpoint.startsWith('/') ? `${API}${endpoint}` : `${API}/${endpoint}`;
+       const url = endpoint.startsWith('http') ? endpoint : `${API}${endpoint}`;
         const response = await fetch(url, {method: 'DELETE'});
         if (!response.ok) throw new Error(`DELETE ${endpoint} Failed`);
         try {
