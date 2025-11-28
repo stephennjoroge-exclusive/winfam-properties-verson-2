88b841da0c3caed5c0e09d93941727046cf737d1
Stephen Njoroge <853stephen@gmail.com>
2025-11-28 16:56:10 +0300

    replaced h-full with flex-1

--- client/src/pages/Chatbot.jsx
+++ client/src/pages/Chatbot.jsx
@@ -26,7 +26,7 @@ const Chatbot = () => {
             Chat with AI
         </div>
 
-        <div className='h-full flex flex-col'>
+        <div className='flex-1 flex flex-col'>
             <div className="flex-1 overflow-y-auto p-4 space-y-2">
                 {messages.map((msg, id) => (
                 <div
