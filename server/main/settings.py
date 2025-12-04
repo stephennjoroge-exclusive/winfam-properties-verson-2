a8695cdfb0eeab774bb3085bf59d12ac3b19fbab
Stephen Njoroge <853stephen@gmail.com>
2025-12-04 10:19:29 +0300

    fixed the debug in the settings

--- server/main/settings.py
+++ server/main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG") == "True"
 
 CORS_ALLOWED_ORIGINS = [
     "https://winfam-properties-verson-2-j1nxzhbk6.vercel.app",
